7ebc71df5f5102da9d323b0665cf07bdc6753268
Thierry Kühni <thierry.kuehni@students.fhnw.ch>
2022-10-06 13:05:34 +0200

    prepare docs to be deployed to github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,10 +14,18 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
 		alias: {
 			'svelte-konva': 'src/lib',
-		}
+		},
+		paths: {
+			base: dev ? '' : '/svelte-konva',
+		},
 	}
 };
 
