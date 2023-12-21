9414de257997b8c39122c10eb88f8cfb404ae5f7
Thierry Kühni <thierry.kuehni@hotmail.com>
2023-12-21 12:11:33 +0100

    Update Sveltekit v2, vite v5, vitest v1, jsdom v23

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,12 @@ const config: UserConfig = {
 	},
 	test: {
 		include: ['src/tests/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-		threads: false, // Workaround as testing with threads causes segfaults (likely vitest bug) Caution: ith this in place tests are not running in isolated threads!
+		pool: "forks", // Workaround as testing with threads causes segfaults (likely vitest bug) Caution: with this in place tests are not running in isolated threads!
 		environment: 'jsdom',
 		environmentOptions: {
 			jsdom: {
 				resources: 'usable'
+
 			}
 		}
 	},
@@ -21,13 +22,13 @@ const config: UserConfig = {
 	// By directly importing the runtime we force Svelte to call it without onMount being replaced by noop
 	resolve: process.env.TEST
 		? {
-				alias: [
-					{
-						find: /^svelte$/,
-						replacement: path.join(__dirname, 'node_modules/svelte/src/runtime/index.js')
-					}
-				]
-			}
+			alias: [
+				{
+					find: /^svelte$/,
+					replacement: path.join(__dirname, 'node_modules/svelte/src/runtime/index.js')
+				}
+			]
+		}
 		: {}
 };
 
