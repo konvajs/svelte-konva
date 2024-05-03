3384c8289ab06a64f319acba5ea2b87537bddb9d
Thierry Kühni <thierry.kuehni@hotmail.com>
2024-05-03 10:32:53 +0200

    Fix test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import type { UserConfig } from 'vite';
-import path from 'path';
 
 const config: UserConfig = {
 	plugins: [sveltekit(), svelteTesting()],
@@ -21,19 +20,7 @@ const config: UserConfig = {
 		alias: {
 			'@testing-library/svelte': '@testing-library/svelte/svelte5'
 		}
-	},
-	// Workaround as in svelte tests onMount is not called due to being in nodejs environment (SSR) see: https://github.com/testing-library/svelte-testing-library/issues/222#issuecomment-1588987135
-	// By directly importing the runtime we force Svelte to call it without onMount being replaced by noop
-	resolve: process.env.TEST
-		? {
-				alias: [
-					{
-						find: /^svelte$/,
-						replacement: path.join(__dirname, 'node_modules/svelte/src/runtime/index.js')
-					}
-				]
-			}
-		: {}
+	}
 };
 
 export default config;
